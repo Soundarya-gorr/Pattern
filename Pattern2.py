fa87715c7fb52dc8e6e12e36bbef61af740fec20
Soundarya <sgorr2327@gmail.com>
2026-07-01 21:59:53 +0530

    modified

--- Pattern2.py
+++ Pattern2.py
@@ -2,7 +2,7 @@ n=9
 for i in range(1, n+1, 1):
     for j in range(1, n+1, 1):
         if j==i :
-        print("*", end="")
+        print("*", end=" ")
     else:
         print("", end="")
         print()
